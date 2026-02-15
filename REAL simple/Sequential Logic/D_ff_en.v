0fd7cc1959809f4212b2d34972793cac6caac315
IAMTAEWOO <gangtaewoo01@gmail.com>
2026-02-15 22:47:19 +0900

    Update D_ff_en.v

--- REAL simple/Sequential Logic/D_ff_en.v
+++ REAL simple/Sequential Logic/D_ff_en.v
@@ -3,15 +3,12 @@ module D_ff_en(
 	input		i_rst_n,
 	input		i_en,
 	input		i_d,
-	output		o_q
+	output	reg	o_q
 );
-	always @(negedge i_rst_n) begin
+	always @(posedge i_clk or negedge i_rst_n) begin
 		if(!i_rst_n)	
 			o_q <= 1'b0;
-	end
-
-	always @(posedge i_clk) begin
-		if(i_en)
+		else if(i_en)
 			o_q <= i_d;
 		else
 			o_q <= o_q;
