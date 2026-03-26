348d3f445f112faf04b816faa6fcbb45ed113820
IAMTAEWOO <gangtaewoo01@gmail.com>
2026-03-26 10:11:39 +0900

    Fix formatting of input and output declarations

--- REAL simple/DigitalSystemDesign-TA/demux_1x4.v
+++ REAL simple/DigitalSystemDesign-TA/demux_1x4.v
@@ -1,10 +1,10 @@
 module demux_1x4(
-  input		[1:0]  i_sel,
-	input		i_x,
-	output		o_y0,
-	output		o_y1,
-	output		o_y2,
-	output		o_y3,
+	input		[1:0]  i_sel,
+	input				i_x,
+	output				o_y0,
+	output				o_y1,
+	output				o_y2,
+	output				o_y3,
 	)
 
   assign o_y0 =  (i_sel  ==  2'b00) ? i_x : 0 ;
